5ee0e6863d9fcae172af72b60d9bef06c01bdb0b
kareemtoson12 <kareemtoson1@gmail.com>
2024-05-02 16:17:13 +0300

    endpoint for images

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection with MongoDB
+mongoose.connect("mongodb+srv://kareemToson:kareemtoson123@cluster0.tr43lhr.mongodb.net/webApp?retryWrites=true&w=majority&appName=Cluster0");
+
+app.get('/', (req, res) => {
+    res.send("Express app is running");
+});
+
+const storage = multer.diskStorage({
+    destination: "./upload/images",
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ storage });
+
+// Create an upload endpoint for images
+app.use('/images', express.static('upload/images'));
+
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server is running on port " + port);
+    } else {
+        console.log("Error: " + error);
+    }
+});
