c1cc204376c76da120b1964707bfb7d2bdd336b4
Marina <rinanashhat64@gmail.com>
2024-05-11 21:47:04 +0300

    fix fetching errors

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import './NewCollections.css';
-import new_collection from '../Assets/new_collections'
-import Item from '../Item/Item'
+import new_collection from '../Assets/new_collections';
+import Item from '../Item/Item';
+import axios from 'axios';
 
 const NewCollections = () => {
-    const [new_collection, setNew_collection] = useState([]);
+    const [newCollections, setNewCollections] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/newcollections')
-            .then((Response)=> Response.json())
-            .then((data)=> setNew_collection(data));
-    })
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/newcollections');
+                setNewCollections(response.data);
+            } catch (error) {
+                console.error("Error fetching new collections:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className='new-collections'>
             <h1>NEW COLLECTIONS</h1>
             <hr />
             <div className="collections">
-                {new_collection.map((item,i)=>{
-                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price}   old_price={item.old_price}/>
+                {newCollections.map((item, i) => {
+                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
                 })}
             </div>
         </div>
